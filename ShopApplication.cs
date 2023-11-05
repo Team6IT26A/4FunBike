55bd300c1b39440adf1e626645b8bdbd6ac79bd6
RotzRohr <mail@linushuck.com>
2023-11-05 23:08:00 +0100

    Update ShopApplication.cs

--- ShopApplication.cs
+++ ShopApplication.cs
@@ -11,48 +11,48 @@ namespace _4FunBike
         private FlowLayoutPanel panel;
 
         public ShopApplication()
+    {
+        InitializeComponent();
+        customItems = new List<CustomItem>();
+
+        // Set up a FlowLayoutPanel to contain the CustomItems and manage their layout
+        panel = new FlowLayoutPanel
         {
-            InitializeComponent();
-            customItems = new List<CustomItem>();
+            Dock = DockStyle.Fill,
+            AutoSize = true,
+            AutoSizeMode = AutoSizeMode.GrowAndShrink,
+            FlowDirection = FlowDirection.TopDown // Stack items vertically
+        };
+        Controls.Add(panel);
 
-            // Set up a FlowLayoutPanel to contain the CustomItems and manage their layout
-            panel = new FlowLayoutPanel
+        // Add example CustomItems
+        for (int i = 1; i <= 5; i++)
+        {
+            CustomItem item = new CustomItem
             {
-                Dock = DockStyle.Fill,
-                AutoSize = true,
-                AutoSizeMode = AutoSizeMode.GrowAndShrink,
-                FlowDirection = FlowDirection.TopDown // Stack items vertically
+                Name = "Bike Model " + i,
+                Price = 99.99m + i, // Example price
             };
-            Controls.Add(panel);
 
-            // Add example CustomItems
-            for (int i = 1; i <= 5; i++)
+            // Attempt to load the image from the file path.
+            try
             {
-                CustomItem item = new CustomItem
-                {
-                    Name = "Bike Model " + i,
-                    Price = 99.99m + i, // Example price
-                };
-
-                // Attempt to load the image from the file path.
-                try
-                {
-                    item.ItemImage = Image.FromFile("C:\\Program Files (x86)\\Steam\\appcache\\librarycache\\730_icon.jpg");
-                }
-                catch (System.IO.FileNotFoundException ex)
-                {
-                    MessageBox.Show("Image file not found: " + ex.Message);
-                    // Set a default image or leave it blank if the image file is not found
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("An error occurred: " + ex.Message);
-                    // Handle other potential exceptions here, such as unauthorized access.
-                }
-
-                AddCustomItem(item);
+                item.ItemImage = Image.FromFile("C:\\Program Files (x86)\\Steam\\appcache\\librarycache\\730_icon.jpg");
             }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                MessageBox.Show("Image file not found: " + ex.Message);
+                // Set a default image or leave it blank if the image file is not found
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+                // Handle other potential exceptions here, such as unauthorized access.
+            }
+
+            AddCustomItem(item);
         }
+    }
 
         // Method to add a CustomItem to the list and panel
         public void AddCustomItem(CustomItem item)
